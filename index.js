5673651ff6813ac08dea2fadedc38b2227c0a322
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2019-12-13 18:57:51 +0200

    feat: implement basic interface

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Type = require('./lib/type');
+const AsyncFunction = (async () => {}).constructor;
+
+const types = {
+  Bigint: new Type(value => typeof value === 'bigint'),
+  Number: new Type(value => typeof value === 'number'),
+  String: new Type(value => typeof value === 'string'),
+  Boolean: new Type(value => typeof value === 'boolean'),
+  Object: new Type(value => typeof value === 'object'),
+  Symbol: new Type(value => typeof value === 'symbol'),
+  Function: new Type(value => typeof value === 'function'),
+  Undefined: new Type(value => typeof value === 'undefined'),
+
+  Set: Type.from(Set),
+  Map: Type.from(Map),
+  Array: Type.from(Array),
+  Date: Type.from(Date),
+  AsyncFunction: Type.from(AsyncFunction),
+  Promise: Type.from(Promise),
+  Buffer: Type.from(Buffer),
+
+  Null: new Type(value => value === null),
+};
+
+module.exports = Object.assign(Type, types);
